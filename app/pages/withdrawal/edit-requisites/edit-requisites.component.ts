8820dd9eee567038a964f5e631435e22bf20287c
Anna Udobnaya <anna.udobnaya@korbit.ru>
2017-08-21 10:52:22 +0200

    one bind way for account edit params

--- app/pages/withdrawal/edit-requisites/edit-requisites.component.ts
+++ app/pages/withdrawal/edit-requisites/edit-requisites.component.ts
@@ -45,9 +45,10 @@ export class EditRequisitesComponent implements OnInit, AfterViewChecked {
                         message: Config.messages.error.body["requisites-error"],
                         okButtonText: Config.messages.button.ok
                     });
-                }
-                if (u.account)
+                } else if (u.account) {
                     this.user.account = u.account;
+                }
+
         });
     }
 
@@ -101,12 +102,13 @@ export class EditRequisitesComponent implements OnInit, AfterViewChecked {
     }
 
      checkBik():boolean{
+       let bic = <TextField>this.bic.nativeElement;
 
-       if (this.user.account.bic.length !== Config.bicLength) {
+       if (bic.text.length !== Config.bicLength) {
            this.errors["bic"] = Config.messages.error.body["bic-length"];
            return false;
        }
-       if (!this.isNumeric(this.user.account.bic)){
+       if (!this.isNumeric(bic.text)){
            this.errors["bic"] = Config.messages.error.body["bic-type"];
            return false;
        }
@@ -114,11 +116,13 @@ export class EditRequisitesComponent implements OnInit, AfterViewChecked {
     }
 
     checkBnkCorr():boolean{
-        if (this.user.account.bnk_corr.length !== Config.bnkCorrLength) {
+        let bncCorr = <TextField>this.bnk_corr.nativeElement;
+
+        if (bncCorr.text.length !== Config.bnkCorrLength) {
             this.errors["bnk_corr"] = Config.messages.error.body["bnk-corr-length"];
             return false;
         }
-        if (!this.isNumeric(this.user.account.bnk_corr)){
+        if (!this.isNumeric(bncCorr.text)){
             this.errors["bnk_corr"] = Config.messages.error.body["bnk-corr-type"];
             return false;
         }
@@ -126,12 +130,14 @@ export class EditRequisitesComponent implements OnInit, AfterViewChecked {
     }
 
     checkFio():boolean{
-        if (!this.user.account.fio.trim()) {
+        let fio = <TextField>this.fio.nativeElement;
+
+        if (!fio.text.trim()) {
             this.errors["fio"] = Config.messages.error.body["fio-length"];
             return false;
         }
 
-        if (~(this.user.account.fio.search(/[0-9]/i))){
+        if (~(fio.text.search(/[0-9]/i))){
             this.errors["fio"] = Config.messages.error.body["fio-type"];
             return false;
         }
